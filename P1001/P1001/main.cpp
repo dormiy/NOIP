748e8c0bca60f1f18129a37ef2ab0a76186495e7
Ping <ping.wuu@gmail.com>
2019-03-02 14:15:37 +0800

    solve the p1001

---
+++ P1001/P1001/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  P1001
+//
+//  Created by 吴平 on 3/2/19.
+//  Copyright © 2019 Ping. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int a, b;
+    cin>>a>>b;
+    cout<<a+b<<endl;
+    return 0;
+}
